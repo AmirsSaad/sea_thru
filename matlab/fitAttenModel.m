90141993c5bf31df77425e358e311b834b4423ed
Ofer Hazut <hazutofer@gmail.com>
2020-03-02 22:26:06 +0200

    model fitting graphs

--- matlab/fitAttenModel.m
+++ matlab/fitAttenModel.m
@@ -3,6 +3,7 @@ coefs=zeros(3,3);
 figure;
 hold on;
 upper_c=min(meanH);
+rgb=['#D95319';'#77AC30';'#0072BD'];
 for i=1:3
     y=meanH(:,i);
     ft = fittype('a*exp(-b*x)+c');
@@ -12,7 +13,12 @@ for i=1:3
     opts.Upper = [inf inf upper_c(i)];
     f1 = fit(z,y,ft,opts);
     coefs(i,1:3)=[f1.a f1.b f1.c];
-    plot(f1,z,y);
+    plot(z,y,'Marker','.','Color',rgb(i,:));
+    plot(f1,'k');
 end
-
+grid minor
+legend('R','B_{model}^R','G','B_{model}^G','B','B_{model}^B','Location','nw');
+xlabel('Distance from camera (z[m])');
+ylabel('Intensity (I[counts])');
+title('Mean Intensity (2%)');
 end
--- matlab/fitBSModel.m
+++ matlab/fitBSModel.m
@@ -4,6 +4,7 @@ coefs=zeros(3,2);
 upper_a=meanH.data(end,2:4);
 figure;
 hold on;
+rgb=['#D95319';'#77AC30';'#0072BD'];
 for i=2:4
     y=lowH.data(:,i);
     ft = fittype('a*(1-exp(-b*(x)))');
@@ -13,7 +14,13 @@ for i=2:4
     opts.Upper = [upper_a(i-1) 5];
     f1 = fit(z,y,ft,opts);
     coefs(i-1,1:2)=[f1.a f1.b];
-    plot(f1,z,y);
-end
+    plot(z,y,'Marker','.','Color',rgb(i-1,:));
+    plot(f1,'k');
 
+end
+grid minor
+legend('R','B_{model}^R','G','B_{model}^G','B','B_{model}^B','Location','nw');
+xlabel('Distance from camera (z[m])');
+ylabel('Intensity (I[counts])');
+title('Lower Percentile Intensity (2%)');
 end
