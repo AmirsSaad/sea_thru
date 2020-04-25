52f1a812bc01774d4f6909060dd9de60b202e5bc
OferHazut <hazutofer@gmail.com>
2020-04-25 17:49:35 +0300

    added whites option

--- matlab/colorModTools/cntStretch.m
+++ matlab/colorModTools/cntStretch.m
@@ -1,13 +1,21 @@
 function Iout = cntStretch(I,method)
+
 if nargin<2
-   method='biside'; %'blacks' 
+   method='biside'; %'blacks' 'whites'
 end
+
 data=rgb2gray(I);
 data=reshape(data,[],1);
-low=prctile(data,1);
-if method=='biside'
+
+if strcmp(method,'biside') || strcmp(method,'blacks')
+    low=prctile(data,1);
+elseif strcmp(method,'whites')
+    low=min(data);
+end
+
+if strcmp(method,'biside') || strcmp(method,'whites')
     high=prctile(data,99);
-elseif method=='blacks'
+elseif strcmp(method,'blacks')
     high=max(data);
 end
 Iout=(I-low)./(high-low);
