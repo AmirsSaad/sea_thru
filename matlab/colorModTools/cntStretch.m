fa86612f128b746def133e62a7243cf9ee0d8243
OferHazut <hazutofer@gmail.com>
2020-04-25 15:42:59 +0300

    added blacks option, goes through gray

--- matlab/colorModTools/cntStretch.m
+++ matlab/colorModTools/cntStretch.m
@@ -1,7 +1,15 @@
-function Iout = cntStretch(I)
-data=reshape(I,[],1);
+function Iout = cntStretch(I,method)
+if nargin<2
+   method='biside'; %'blacks' 
+end
+data=rgb2gray(I);
+data=reshape(data,[],1);
 low=prctile(data,1);
-high=prctile(data,99);
+if method=='biside'
+    high=prctile(data,99);
+elseif method=='blacks'
+    high=max(data);
+end
 Iout=(I-low)./(high-low);
 Iout(Iout<0)=0;
 Iout(Iout>1)=1;
